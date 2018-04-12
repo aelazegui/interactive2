a492b09f8858a5cd2725ccd546e8bb03deef728c
aelazegui <31681667+aelazegui@users.noreply.github.com>
2018-04-12 16:00:23 -0700

    upload to github

---
+++ Exhibition/Impressions-of-South-Africa/Code/assets/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	
+
+$('#savethepress').on('click', function (event) {
+	$("#savethepress").addClass('comeforward');
+	$("#savethepress").removeClass('pushback');
+	});
+	
+var artwork = document.querySelectorAll(Image);
+var zValue = document
+	
+	
+	function getArtwork() {
+		var artwork = document.querySelectorAll(Image);
+		artwork.click(function)
+		
+		
+		element.classList.toggle("mystyle");
+	
+//$(function() {
+//    var boxes = $("photo");
+//    boxes.click(function() {
+//        var el = $(this), 
+//            max = 0;
+//        boxes.each(function() {
+//            var z = parseInt( $( this ).css( "z-index" ), 10 );
+//            max = Math.max( max, z );
+//        });
+//        el.css("z-index", max + 1 );
+//    });
+//});
+
+
+//function myFunction() {
+//    var element = document.getElementById("savethepress");
+//    element.classList.toggle("mystyle");
+//}
+//
+//  $("p").click(function(){
+//        $(this).hide();
+//    });
+
+//$("img").click(function(){
+//    document.getElementById("img").style.zIndex = "100"
+//});
+	
+	
+//document.getElementById("img1").style.zIndex = "100"
+	
+	
+});
+
+//maybe on img click, a duplicate comes forward & opacity increases
+//and the actual one opacity 0 while the group pushes into background.
