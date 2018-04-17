07d46173d3825bf112113ea4d5fb84705952e533
aelazegui <31681667+aelazegui@users.noreply.github.com>
2018-04-17 16:13:55 -0700

    upload to github

--- Exhibition/Impressions-of-South-Africa/Code/assets/script.js
+++ Exhibition/Impressions-of-South-Africa/Code/assets/script.js
@@ -1,22 +1,163 @@
 $(document).ready(function(){
 	
-
-$('#savethepress').on('click', function (event) {
-	$("#savethepress").addClass('comeforward');
-	$("#savethepress").removeClass('pushback');
+	
+	var images = $('img');	
+	var forwardnow = $('img').has('comeforward');
+	
+	
+	$("img:not('comeforward')").hover( 
+		function (){ // Mouse over
+		$(this).addClass("hoverimg");
+	}, function() { // Mouse out
+  		$(this).removeClass("hoverimg");
 	});
 	
-var artwork = document.querySelectorAll(Image);
-var zValue = document
+	
+	$('img').on('click', function (){
+		if ($('img').hasClass('comeforward')) {
+			
+			//$(this).addClass("makeroom");
+			$(this).animate({top: '50px', left: '50px'}, 1500, function () {
+			console.log('i moved it!');
+	});
+		} else {
+			$('img:not(this)')
+				.addClass("pushback")
+				//.addClass('blurimg');
+			$(this).addClass("comeforward");
+		}
+	});
 	
 	
-	function getArtwork() {
-		var artwork = document.querySelectorAll(Image);
-		artwork.click(function)
+//	
+//	$('img.comeForward').on('click', function(){ 
+//		$(this).addClass('makeRoom');
+//	});
+	
+	$('#savethepress.comeforward').click(function(){
+		$(".worktitles .bodycopy").fadeIn();
+	});
+	
+	
+	
+	
+//	$("img:not('comeforward')").hover( function (){ // Mouse over
+//		$(this).addClass("makeroom");
+//	}, function() { // Mouse out
+//  		$(this).removeClass("makeroom");
+//});
+	
+		
+	
+//	$('img').on('click', function (){
+//		//all images will push back
+//  	$('img:not(this)').addClass("pushback");
+//		//the image clicked on will come forward
+//	$(this).addClass("comeforward");
+//	});	
+//	
+//	
+//	
+//	//on click, images that already have comeforeword applied to it
+//	
+
+//	
+//	
+//	
+//	
+//	
+//if ($(this).is('comeforward')) { 		//if the thing clicked is already 'comefoward'
+//			$(this).on('click', function (){ //then on click, animate it
+//				$(this).animate({
+//        			left: '250px',
+//        			opacity: '0.5',
+//					height: '150px',
+//        			width: '150px'	
+//	
+//	
+	
+//	images.on('click', function (){
+//		$(this).toggleClass('comeforward');
+//		console.log ('toggleme');
+//		if ($(this).is('comeforward')) {
+//			images.not(this).each(function(){
+//         	$(this).addClass('pushback');
+//			$(this.target).animate(({height:'hide', opacity:'hide'}, slidespeed));
+//             });
+//			$(this).removeClass('pushback');
+//	});
+	
+	
+	
+//	
+//		
+//		if ($(this).is('pushback')) {
+//			$(this).removeClass("pushback");
+//			$(this).addClass("comefoward");
+//			images.addClass("pushback");}
+//	});	
+//	
+	
+	
+	
+	
+	
+//	images.hover(function (){
+//		$(this).addClass('hoverrise');
+//	}, function() {
+//		$(this).removeClass('hoverrise');
+//	}
+//	});
+	
+	
+	
+	
+	
+//$('#savethepress').on('click', function (event) {
+//	$("#savethepress").addClass('comeforward');
+//	$("#savethepress").removeClass('pushback');
+//	});
+//
+//document.getElementsByClassName('.photo').on('click',function(event) {
+//	
+//} 
+//											 
+//var artwork = 
+//	
+//	
+//function addOneToZ (){
+//	document.querySelectorAll(Image).style.zIndex = + 1;
+//	}
+//	
+//	
+//	function getArtwork() {
+//		var artwork = document.querySelectorAll('Image');
+//		artwork.click(function)
+//		
+//		
+//		element.classList.toggle("mystyle");
+////	
+//		
+//var allArtwork = document.querySelectorAll(Image);
+//var zValue = allArtwork.style.zIndex;
+//
+//		
+//$('Struckarock').on('click', function (event) {
+//
+//if (allArtwork.style.zIndex >= 100)
+//	
+//	
+//	allArtwork.style.zIndex = 100;
+//	
+//	if (allArtwork.style.zIndex > 100) {
+//	allArtwork.style.zIndex = - 1;
+//	}
+//		
+//		
 		
 		
-		element.classList.toggle("mystyle");
-	
+		
+		
 //$(function() {
 //    var boxes = $("photo");
 //    boxes.click(function() {
